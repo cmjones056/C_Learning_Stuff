435acef09aca9e6249d2011465217d4383437f65
Tyler Hovanec <tbhovanec@student.ysu.edu>
2018-05-07 20:57:23 -0700

    Added GoogleTest library and test cases for a vector class.

---
+++ vector.cpp
@@ -0,0 +1,14 @@
+#include "vector.h"
+
+namespace colleen {
+
+// The constructor does nothing for now...
+vector::vector() {
+
+}
+
+bool vector::empty() {
+  return true;
+}
+
+}  //namespace colleen
